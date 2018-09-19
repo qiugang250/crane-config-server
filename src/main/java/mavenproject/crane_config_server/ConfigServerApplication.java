96d0bbd04adf668e833f7380de847ff711c1a08e
qiugang <849888900@qq.com>
2018-09-19 11:21:10 +0800

    create

---
+++ src/main/java/mavenproject/crane_config_server/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package mavenproject.crane_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
+public class ConfigServerApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
